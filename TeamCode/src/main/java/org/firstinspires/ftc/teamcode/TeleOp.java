f6b9b7bb70a09d6c5cac27334ab3b96344bd1523
TheMrStealth <90276412+TheMrStealth@users.noreply.github.com>
2025-09-18 21:24:10 -0400

    messed some stuff up before transfer. oops

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Manual");
+public class TeleOp extends OpMode{
+
+}
