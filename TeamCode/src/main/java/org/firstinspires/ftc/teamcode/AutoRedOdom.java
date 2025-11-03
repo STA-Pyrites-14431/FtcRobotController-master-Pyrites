48855cd2d1192eac2882b668bf114130f379f7d3
TheMrStealth <90276412+TheMrStealth@users.noreply.github.com>
2025-11-03 15:47:56 -0500

    started encoders code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedOdom.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class AutoRedOdom extends LinearOpMode {
+
+    BotDrive drive = new BotDrive();
+
+    DcMotor motorFL; //FrontLeft motor
+    DcMotor motorFR; //FrontRight motor
+    DcMotor motorBL; //BackLeft motor
+    DcMotor motorBR; //BackRight motor
+    DcMotor motorLL; //LauncherLeft motor
+    DcMotor motorLR; //LauncherRight motor
+    DcMotor motorI; //Intake motor
+    CRServo servoR1; //Ramp1 servo
+    CRServo servoR2; //Ramp2 servo
+    CRServo servoR3; //Ramp3 servo
+
+    float axial;
+    float lateral;
+    float yaw;
+    float powerFL;
+    float powerFR;
+    float powerBL;
+    float powerBR;
+    float max;
+    double ticks = 537.7;
+    double newTarget;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorFL = hardwareMap.get(DcMotor.class,"motorFL"); //EH0
+        motorFR = hardwareMap.get(DcMotor.class,"motorFR"); //CH0
+        motorBL = hardwareMap.get(DcMotor.class,"motorBL"); //EH1
+        motorBR = hardwareMap.get(DcMotor.class,"motorBR"); //CH1
+        motorLR = hardwareMap.get(DcMotor.class,"motorLR"); //CH2
+        motorLL = hardwareMap.get(DcMotor.class,"motorLL"); //EH2
+        motorI = hardwareMap.get(DcMotor.class,"motorI"); //CH3
+        servoR1 = hardwareMap.get(CRServo.class,"servoR1"); //EH0
+        servoR2 = hardwareMap.get(CRServo.class,"servoR2"); //EH1
+        servoR3 = hardwareMap.get(CRServo.class,"servoR3"); //EH2
+
+
+    }
+}
